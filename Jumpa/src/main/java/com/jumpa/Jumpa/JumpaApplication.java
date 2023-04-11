6f71d4071429f59f79de1cfdd8ce729a497a69be
Ricardo Castro Vizcaya <36154504+RicardoCastroVizcaya@users.noreply.github.com>
2023-04-11 17:19:05 +0200

    SpringBoot Initializer

--- Jumpa/src/main/java/com/jumpa/Jumpa/JumpaApplication.java
+++ Jumpa/src/main/java/com/jumpa/Jumpa/JumpaApplication.java
@@ -2,12 +2,25 @@ package com.jumpa.Jumpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class JumpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JumpaApplication.class, args);
+	}
+}
+
+@Controller
+class HomeController {
+
+	@GetMapping("/")
+	public String homePage(Model model) {
+		model.addAttribute("name", "Dani");
+		return "hello";
 	}
 
 }
