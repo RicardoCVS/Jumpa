7fac4436abc116dc7a7d9f1ac943cbf169f9b76f
pespinas <77834644+pespinas@users.noreply.github.com>
2023-04-20 18:24:54 +0200

    Actualización

--- src/main/jumpa-react/src/App.js
+++ src/main/jumpa-react/src/App.js
@@ -1,23 +1,11 @@
 import './Tarjeta.css';
-import Card from './Tarjeta.js';
+import Indice from './indice.js'
 
 function App() {
-    const rutaImagen = process.env.PUBLIC_URL + '/img/samuel.jpg';
-    const rutaImagen2 = process.env.PUBLIC_URL + '/img/ball.png';
+   
   return (
     <>
-    <Card 
-      titleFront="Viva el betis"imagenBack={rutaImagen}
-      titleBack="asd" textBack="Pipsa,Pipsa,Pipsa,Pipsa,Pipsa" imagenfront={rutaImagen2}
-    />
-    <Card 
-      titleFront="Viva el betis" imagenBack={rutaImagen}
-      titleBack="asd" textBack="Pipsa,Pipsa,Pipsa,Pipsa,Pipsa Pipsa Pipsa Pipsa PipsaPipsaPipsaPipsaPipsaPipsa" 
-    />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+    <Indice />
     
   </>
   );
