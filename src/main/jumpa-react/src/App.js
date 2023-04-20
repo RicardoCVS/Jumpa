35f6227b872ae6c6986269588c5694a525422ab6
DanielHernandez8 <124901241+DanielHernandez8@users.noreply.github.com>
2023-04-20 18:46:40 +0200

    20-4

--- src/main/jumpa-react/src/App.js
+++ src/main/jumpa-react/src/App.js
@@ -1,14 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import './inicio.js';
-import './menu-flotante.js';
 import Title from './title.js';
 import Inicio from './inicio.js';
-import Menu from './menu-flotante';
 function App() {
   return (
     <>
-    <Menu/>
     <Title />
     <Inicio />
     </>
