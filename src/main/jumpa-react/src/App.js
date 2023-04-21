181dac5fbb034e6112e7718e3cf4d8670460099d
pespinas <77834644+pespinas@users.noreply.github.com>
2023-04-21 19:43:19 +0200

    update

--- src/main/jumpa-react/src/App.js
+++ src/main/jumpa-react/src/App.js
@@ -1,16 +1,16 @@
 import './Assets/inicio.js';
+import './App.css'
 import Title from './Assets/title.js';
 import Inicio from './Assets/inicio.js';
-import './Assets/Tarjeta.css';
 import Indice from './Assets/indice.js'
 
 function App() {
    
   return (
     <>
-    <Title />
-    <Inicio />
-    <Indice />
+      <Title />
+      <Inicio />
+      <Indice />
     </>
   );
   
