14701fbd17a04f88d16341e3a6d15cbe5dfa3f4e
Ricardo Castro Vizcaya <36154504+RicardoCastroVizcaya@users.noreply.github.com>
2023-05-02 18:14:31 +0200

    Cambios to flamas

--- src/main/resources/static/dragDropGame.js
+++ src/main/resources/static/dragDropGame.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
     const trashItems = document.querySelectorAll('img[draggable="true"]');
     const bins = document.querySelectorAll('.bin');
+    const message = document.getElementById('message');
+    let correctCount = 0;
 
     trashItems.forEach(item => {
         item.addEventListener('dragstart', handleDragStart);
+        item.addEventListener('dragend', handleDragEnd);
     });
 
     bins.forEach(bin => {
@@ -13,10 +16,17 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function handleDragStart(e) {
         e.dataTransfer.setData('text/plain', e.target.id);
+        e.dataTransfer.setDragImage(e.target, e.target.width / 2, e.target.height / 2);
+        e.dataTransfer.effectAllowed = 'move';
+    }
+
+    function handleDragEnd(e) {
+        e.target.style.opacity = '1';
     }
 
     function handleDragOver(e) {
         e.preventDefault();
+        e.dataTransfer.dropEffect = 'move';
     }
 
     function handleDrop(e) {
@@ -28,7 +38,10 @@ document.addEventListener('DOMContentLoaded', function () {
         if (itemId.includes(binType)) {
             e.target.appendChild(item);
             item.setAttribute('draggable', 'false');
+            correctCount++;
+            if (correctCount === trashItems.length) {
+                message.innerHTML = '¡Bien hecho!';
+            }
         }
     }
 });
-
