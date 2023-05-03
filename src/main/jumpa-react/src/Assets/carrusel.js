f4fffa987f81f6d2ac2cb7077038660671bc54c5
DanielHernandez8 <124901241+DanielHernandez8@users.noreply.github.com>
2023-05-03 17:04:46 +0200

    imagen fundacio

--- src/main/jumpa-react/src/Assets/carrusel.js
+++ src/main/jumpa-react/src/Assets/carrusel.js
@@ -26,7 +26,7 @@ function lospuntos(){
 
 function Carrusel(){
     const rutaImagen = process.env.PUBLIC_URL + '/img/samuel.jpg';
-    const fundacio = process.env.PUBLIC_URL + '/img/fundacio.jpg';
+    const fundacio = process.env.PUBLIC_URL + '/img/fundacio.png';
     const rutaImagen2 = process.env.PUBLIC_URL + '/img/ball.png';
     const idea = process.env.PUBLIC_URL + '/img/ideas.jpg';
     const tec = process.env.PUBLIC_URL + '/img/tecnologia.jpg';
