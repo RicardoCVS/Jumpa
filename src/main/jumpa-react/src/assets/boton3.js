29877f7ba3ee38670aa72b4cfc5fc550c29b4895
pespinas <77834644+pespinas@users.noreply.github.com>
2023-05-10 19:26:46 +0200

    update

--- src/main/jumpa-react/src/assets/boton3.js
+++ src/main/jumpa-react/src/assets/boton3.js
@@ -8,7 +8,7 @@ function Boton3() {
 
   return (
     <div class="wrap" onClick={handleClick}>
-      <button class="button">JUMPA</button>
+      <button class="button3">JUMPA</button>
     </div>
   );
 }
