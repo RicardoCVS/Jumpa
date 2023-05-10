3b082aab1ba7f76e18373a072fec0da564eb1eb4
Ricardo Castro Vizcaya <36154504+RicardoCastroVizcaya@users.noreply.github.com>
2023-05-10 15:48:39 +0200

    Implementados los mappings de juegos 1,2,3,4

--- src/main/java/com/jumpa/Jumpa/controller/JuegoController.java
+++ src/main/java/com/jumpa/Jumpa/controller/JuegoController.java
@@ -11,16 +11,43 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/api/juegos")
+@RequestMapping("/api")
 public class JuegoController {
 
-    @GetMapping
+    @GetMapping("/juegos")
     public ResponseEntity<Resource> showGamePage() {
         Resource resource = new ClassPathResource("static/game.html");
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.TEXT_HTML);
         return new ResponseEntity<>(resource, headers, HttpStatus.OK);
     }
+
+    @GetMapping("/juegos1")
+    public ResponseEntity<Resource> showGamePage1() {
+        Resource resource = new ClassPathResource("static/game1.html");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.TEXT_HTML);
+        return new ResponseEntity<>(resource, headers, HttpStatus.OK);
+    }
+
+    @GetMapping("/juegos2")
+    public ResponseEntity<Resource> showGamePage2() {
+        Resource resource = new ClassPathResource("static/game2.html");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.TEXT_HTML);
+        return new ResponseEntity<>(resource, headers, HttpStatus.OK);
+    }
+
+    @GetMapping("/juegos3")
+    public ResponseEntity<Resource> showGamePage3() {
+        Resource resource = new ClassPathResource("static/game3.html");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.TEXT_HTML);
+        return new ResponseEntity<>(resource, headers, HttpStatus.OK);
+    }
 }
+
+
+
 
 
